aaa6206d85af727e754620afa9859f7d425c1510
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2022-06-08 19:26:14 -0300

    Counting stable

--- saveSbornia.py
+++ saveSbornia.py
@@ -66,27 +66,16 @@ def selectonSort(inputArray, n):
         inputArray[less]=aux
     return inputArray
 
-def countingSort(inputArray, maxElement):
-
+def countingSortStable(inputArray, maxElement):
     countArrayLength = maxElement+1
-
-    # Initialize the countArray with (max+1) zeros
     countArray = [0] * countArrayLength
 
-    # Step 1 -> Traverse the inputArray and increase
-    # the corresponding count for every element by 1
     for el in inputArray:
         countArray[el.get('log')] += 1
 
-    # Step 2 -> For each element in the countArray,
-    # sum up its value with the value of the previous
-    # element, and then store that value
-    # as the value of the current element
     for i in range(1, countArrayLength):
         countArray[i] += countArray[i-1]
 
-    # Step 3 -> Calculate element position
-    # based on the countArray values
     outputArray = [0] * len(inputArray)
     i = len(inputArray) - 1
     while i >= 0:
